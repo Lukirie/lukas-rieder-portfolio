c0b84184830f9eba3075250fc98976c0d02c99fb
Lukas Rieder <riederlukas01@gmail.com>
2026-01-28 00:26:39 +0100

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/lukas-rieder-portfolio/" : "/",
   server: {
     host: "::",
     port: 8080,
