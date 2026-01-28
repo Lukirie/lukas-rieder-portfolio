7f0cc9ce75120455f7e04b3f573d770faf8b757f
Lukas Rieder <riederlukas01@gmail.com>
2026-01-29 00:51:46 +0100

    lovable dep

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/lukas-rieder-portfolio/" : "/",
+export default defineConfig({
+  base: "/lukas-rieder-portfolio/",
   server: {
     host: "::",
     port: 8080,
@@ -13,10 +12,10 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
