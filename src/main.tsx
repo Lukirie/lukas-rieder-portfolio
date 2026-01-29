e152785c099fe9b3602e66ab0fc85b114e1a1843
Lukas Rieder <riederlukas01@gmail.com>
2026-01-29 02:07:36 +0100

    Suppress feature_collector third-party warnings

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Suppress third-party warnings
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (typeof args[0] === 'string' && args[0].includes('feature_collector')) {
+    return; // Suppress feature_collector warnings
+  }
+  originalWarn(...args);
+};
+
 createRoot(document.getElementById("root")!).render(<App />);
